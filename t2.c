96a72a663604954302c3731f3faacd0b4230341d
tanakorn0314 <tanakorn0412@gmail.com>
2018-04-06 20:44:02 +0700

    Create t2.c

---
+++ t2.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+int main()
+ { 
+    DDRB = 0xff;
+    TIMSK1 = 0x01;
+  
+    TCCR1A = 0x40; //0100 0000
+    TCCR1B = 0x0C; //0000 1100
+     TCNT1 = 0;
+    OCR1A = 780;
+   while (1);
+   return 0;
+ }
+
